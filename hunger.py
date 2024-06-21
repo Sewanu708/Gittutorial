822d6502e8530003958c5ef7957aca402a236f04
Sewanu708 <Sewanu708@gmail.com>
2024-06-21 21:26:11 +0100

    This is my fourth commit

--- hunger.py
+++ hunger.py
@@ -5,9 +5,26 @@ def hunger(Status):
         return 'Im Okay!'
     else:
         return 'I want to go home!'
+
+class placement:
+    def __init__(self,level):
+        if level==400:
+            self.training = True
+        else:
+            self.training = False
+
+    def department(self,department):
+        if len(department) > 9: 
+            return f'department is {self.training}'
+        else:
+            return 'Go home!'
+
+
     
 x=int(input('Input Either 0 or 1 to determine your hunger status'))
 
 print(hunger(x))
 
+y=placement(500)
+print(y.department('Biology'))
 
