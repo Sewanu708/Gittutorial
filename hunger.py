843fb56577c14350c43ad471c5b6590781b1ea0b
Sewanu708 <Sewanu708@gmail.com>
2024-06-21 21:24:30 +0100

    Revert "This is my third commit"
    This reverts commit a18b2111227adf68a4093fbf764911b9eba5e346.

--- hunger.py
+++ hunger.py
@@ -5,26 +5,9 @@ def hunger(Status):
         return 'Im Okay!'
     else:
         return 'I want to go home!'
-
-class placement:
-    def __init__(self,level):
-        if level==400:
-            self.training = True
-        else:
-            self.training = False
-
-    def department(self,department):
-        if len(department) > 6: 
-            return f'department is {self.training}'
-        else:
-            return 'Go home!'
-
-
     
 x=int(input('Input Either 0 or 1 to determine your hunger status'))
 
 print(hunger(x))
 
-y=placement(500)
-print(y.department('Biology'))
 
